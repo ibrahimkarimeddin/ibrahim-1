9d06b321ae4c25fb34a334446fafea541a6990d8
ibrahimkarimeddin <ibrahimkarimeddin@gmail.com>
2022-05-25 06:19:13 -0700

    first

--- components/Header.js
+++ components/Header.js
@@ -10,12 +10,12 @@ import { HomeIcon ,
 function Header() {
   return (
     <header className='flex flex-col items-center sm:flex-row m-5 justify-between h-auto'>
-        <div className='flex flex-grow justify-evenly max-w-2xl'>
+        <div className='flex flex-grow justify-center max-w-2xl'>
             <Headeritem  title='Home' Icon={HomeIcon}/>
-            <Headeritem  title='Light' Icon={LightningBoltIcon}/>
+            {/* <Headeritem  title='Light' Icon={LightningBoltIcon}/>
             <Headeritem  title='User' Icon={UserIcon}/>
             <Headeritem  title='Collection' Icon={CollectionIcon}/>
-            <Headeritem  title='Check' Icon={BadgeCheckIcon}/>
+            <Headeritem  title='Check' Icon={BadgeCheckIcon}/> */}
             <Headeritem  title='Search' Icon={SearchIcon}/>
         </div>
         <h1 className='text-3xl font-bold sm:mb-5 sm:mr-3'>Ibrahim</h1>
